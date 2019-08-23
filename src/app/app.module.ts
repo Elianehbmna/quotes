144e41a57b44fb87c890d2ad6c4e642196c8d5a6
Elianehbmna <elianehabimana3@gmail.com>
2019-08-23 09:31:11 +0200

    adding highlights and time counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { HighlightDirective } from './highlight.directive';
+import { TimeAgoPipe } from './time-ago.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailComponent,
+    HighlightDirective,
+    TimeAgoPipe
   ],
   imports: [
     BrowserModule,
