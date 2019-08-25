bfccfc4a3605794a0944a61a5980ec939c7cf2a4
Elianehbmna <elianehabimana3@gmail.com>
2019-08-25 18:15:16 +0200

    removing toggle

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,8 @@
 import { Variable } from '@angular/compiler/src/render3/r3_ast';
 
 export class Quote {
-  //public showDescription: boolean;
+
   constructor(public id: number,public description: string,public name: string,public Aname: string,public Upvote: number,public Downvote: number, public completeDate: Date){
-    //this.showDescription=false;
+    
   }
 }
